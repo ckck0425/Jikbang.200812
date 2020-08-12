4799327213d4eaa7a56288af0e343bf59d73d346
yugyeong ko <ckck0425ko@gmail.com>
2020-08-12 19:56:59 +0900

    baseactivity 로 코드 정리 기초작업 수행

--- app/src/main/java/kr/co/tjoeun/jikbang200812/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/jikbang200812/MainActivity.kt
@@ -3,10 +3,27 @@ package kr.co.tjoeun.jikbang200812
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+
+    override fun setupEvents() {
+
+//        메인화면의 이벤트 관련 코드 모아둘 장소
+
+    }
+
+    override fun setValues() {
+
+//        메인화면의 값 출력 관련 코드 모아둘 장소
+    }
+
+
+
 }
